7f9255e0eb8dfc981a729ef4c08b7791496a0713
smit_patwa <smit.patwa@gmail.com>
2015-09-19 15:05:51 +0530

    added boolean functions improved same type detection

--- sp.pl
+++ sp.pl
@@ -1,19 +1,37 @@
 int(smit).
 int(deep).
 int([]).
-int(plus([])).
-int(mul([])).
 int([X|Y]):- int(X),int(Y).
-int(plus([X|Y])):- int(X),int(Y).
-int(mul([X|Y])):- int(X),int(Y).
-plus([X|Y]):- int(X),int(Y);my_float(X),my_float(Y).
-mul([X|Y]):- int(X),int(Y);my_float(X),my_float(Y).
+int(plus(X)):- int(expr(X)).
+int(mul(X)):- int(expr(X)).
+int(sub(X)):- int(expr(X)).
+int(div(X,Y)):- int(expr(X)),int(expr(Y)).
+int(expr([X|Y])):- int(X),int(Y).
 
-my_float(daivik).
-my_float([]).
-my_float(plus([])).
-my_float([X|Y]):- my_float(X),my_float(Y).
-my_float(plus([X|Y])):- my_float(X),my_float(Y).
-my_float(mul([X|Y])):- my_float(X),my_float(Y).
+floatF(daivik).
+floatF([]).
+floatF([X|Y]):- floatF(X),floatF(Y).
+floatF(plus([X|Y])):- floatF(X),floatF(Y).
+floatF(mul([X|Y])):- floatF(X),floatF(Y).
+floatF(sub([X|Y])):- floatF(X),floatF(Y).
+floatF(div(X,Y)):- floatF(X),floatF(Y).
 
-equals(A,B):- (my_float(A),my_float(B));int(A),int(B).
+sameType(E1,E2):- (floatF(E1),floatF(E2));(int(E1),int(E2));(boolean(E1),boolean(E2)).
+equals(A,B):- sameType(A,B).
+ifElse(E1,E2,E3):-boolean(E1),sameType(E2,E3).
+
+boolean(true).
+boolean(false).
+boolean([]).
+boolean([X|Y]):- boolean(X),boolean(Y).
+boolean(greater(X,Y)):-sameType(X,Y).
+boolean(greaterEqual(X,Y)):-sameType(X,Y).
+boolean(less(X,Y)):-sameType(X,Y).
+boolean(lessEqual(X,Y)):-sameType(X,Y).
+boolean(equalEqual(X,Y)):-sameType(X,Y).
+boolean(notEqual(X,Y)):-sameType(X,Y).
+boolean(and([A|B])):-boolean(A),boolean(B).
+boolean(or([A|B])):-boolean(A),boolean(B).
+boolean(not(A)):-boolean(A).
+
+%# +, -, *, /, %, <, <=, >, >=, ==, !=
